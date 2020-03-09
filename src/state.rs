57f8af0f12e7223a352a5d926a30480873728dff
Fangdun Cai <cfddream@gmail.com>
2020-03-09 09:50:00 +0800

    docs: store & state

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,7 @@
 //! State Map
 //!
 //! Stores the value by the key.
+//! Based on [`serde_json::Map`].
 
 use serde_json::{Map, Value};
 
--- src/store.rs
+++ src/store.rs
@@ -7,10 +7,13 @@ use std::{fmt, future::Future, io::Error, pin::Pin};
 use crate::Session;
 
 pub trait Storable: Send + Sync + 'static {
+    /// Gets a session by the id.
     fn get(&self, id: &str) -> Pin<Box<dyn Future<Output = Result<Session, Error>> + Send + '_>>;
 
+    /// Removes a session by the id.
     fn remove(&self, id: &str) -> Pin<Box<dyn Future<Output = Result<(), Error>> + Send + '_>>;
 
+    /// Saves a session.
     fn save(
         &self,
         session: &Session,
@@ -18,6 +21,7 @@ pub trait Storable: Send + Sync + 'static {
 
     /// @TODO: encode & decode the state
 
+    /// Just hacks for debuging the Store.
     fn debug(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result;
 }
 
