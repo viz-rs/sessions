226acb6ade5ec62023dd23ba64c4abee9b89698f
Fangdun Tsai <cfddream@gmail.com>
2022-07-02 15:02:25 +0800

    Release 0.3.1
    sessions@0.3.1
    sessions-core@0.3.1
    sessions-memory@0.3.1
    sessions-redis@0.3.1
    
    Generated by cargo-workspaces

--- sessions-core/src/config.rs
+++ sessions-core/src/config.rs
@@ -3,12 +3,7 @@ use std::{fmt, ops::Deref};
 use crate::Storage;
 
 /// Sessions Config
-pub struct Config<S, G, V>
-where
-    S: Storage,
-    G: Fn() -> String,
-    V: Fn(&str) -> bool,
-{
+pub struct Config<S, G, V> {
     /// Current Storage
     pub storage: S,
     /// Generates session id
@@ -39,34 +34,19 @@ where
     }
 }
 
-impl<S, G, V> fmt::Debug for Config<S, G, V>
-where
-    S: Storage,
-    G: Fn() -> String,
-    V: Fn(&str) -> bool,
-{
+impl<S, G, V> fmt::Debug for Config<S, G, V> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Config").finish()
     }
 }
 
-impl<S, G, V> AsRef<S> for Config<S, G, V>
-where
-    S: Storage,
-    G: Fn() -> String,
-    V: Fn(&str) -> bool,
-{
+impl<S, G, V> AsRef<S> for Config<S, G, V> {
     fn as_ref(&self) -> &S {
         &self.storage
     }
 }
 
-impl<S, G, V> Deref for Config<S, G, V>
-where
-    S: Storage,
-    G: Fn() -> String,
-    V: Fn(&str) -> bool,
-{
+impl<S, G, V> Deref for Config<S, G, V> {
     type Target = S;
 
     fn deref(&self) -> &S {
